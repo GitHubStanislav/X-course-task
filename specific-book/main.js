bb9765a7d6c94a75e118222acfc14f58b7f5dd96
Stanislav Spiridonov <spiridonov.kyiv@gmail.com>
2023-03-14 18:49:11 +0200

    Revert "Refactoring/js"

--- specific-book/main.js
+++ specific-book/main.js
@@ -1,21 +1,23 @@
 "use strict";
-const imgButtonsForForm = document.querySelector(".svg-wrapper");
-const formPriseValue = document.querySelector(".prise");
-const formTotalPrise = document.querySelector(".total-prise");
-const inputFormOrdering = document.querySelector("#input-count");
-const inputsFor42MaxLogic = document.querySelectorAll("#input-count");
+const imgWrapper = document.querySelector(".svg-wrapper");
+const prise = document.querySelector(".prise");
+const totalPrise = document.querySelector(".total-prise");
+const input = document.querySelector("#input-count");
+const inputs = document.querySelectorAll("#input-count");
+const imgUp = document.querySelector(".img-svg-up");
+const imgDown = document.querySelector(".img-svg-down");
+const button = document.querySelector(".my-button-style");
 
-
-const inputTotalPrise = function () {
-  return Number(inputFormOrdering.value) * Number(formPriseValue.innerHTML);
-
+let inputTotalPrise = function () {
+  let resault = Number(input.value) * Number(prise.innerHTML);
+  return resault;
 };
 
-Array.from(inputsFor42MaxLogic).forEach((input) => {
+Array.from(inputs).forEach((input) => {
   const min = +input.min;
   const max = +input.max;
 
-  input.addEventListener("input", () => {
+  input.addEventListener("input", (e) => {
     const value = +input.value;
     if (value > max) {
       input.value = max;
@@ -25,21 +27,21 @@ Array.from(inputsFor42MaxLogic).forEach((input) => {
   });
 });
 
-inputFormOrdering.addEventListener("input", () => {
-  formTotalPrise.innerHTML = inputTotalPrise();
+input.addEventListener("input", () => {
+  totalPrise.innerHTML = inputTotalPrise();
 });
 
-imgButtonsForForm.addEventListener("click", (event) => {
+imgWrapper.addEventListener("click", (event) => {
   if (event.target && event.target.matches("img.img-svg-up")) {
-    if (Number(inputFormOrdering.value) !== 42) {
-      inputFormOrdering.value++;
-      formTotalPrise.innerHTML = inputTotalPrise();
+    if (Number(input.value) !== 42) {
+      input.value++;
+      totalPrise.innerHTML = inputTotalPrise();
     }
   }
   if (event.target && event.target.matches("img.img-svg-down")) {
-    if (Number(inputFormOrdering.value) !== 1) {
-      formTotalPrise.innerHTML = (inputTotalPrise() - Number(formPriseValue.innerHTML)).toString()
-      inputFormOrdering.value--;
+    if (Number(input.value) !== 1) {
+      totalPrise.innerHTML = inputTotalPrise() - Number(prise.innerHTML);
+      input.value--;
     }
   }
 });
